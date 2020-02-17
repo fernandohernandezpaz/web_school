ab683c98cd3fbb38d71fad9eb92c3ae4cd31126d
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2020-02-16 22:09:58 -0600

    agregando modelo docente al administrador de django

--- school/admin.py
+++ school/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from school.models import Person
+from school.models import Person, Teacher
 
 # Register your models here.
 admin.site.register(Person)
+admin.site.register(Teacher)
