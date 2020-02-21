5d402875a6c6516eb6a5aad89232db7453c6fcf0
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2020-02-20 20:42:34 -0600

    creando modelo Profile(Perfil)

--- school/models.py
+++ school/models.py
@@ -13,3 +13,24 @@ class Nationality(models.Model):
     class Meta:
         verbose_name = 'Nacionalidad'
         verbose_name_plural = 'Nacionalidades'
+
+
+class Profile(models.Model):
+    VOCATIONAL_CHOICES = [
+        ('P', 'Primaria'),
+        ('S', 'Secundaria'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Usuario')
+    document = models.CharField(max_length=16, unique=True, null=False, blank=False, verbose_name='Cedula')
+    vocation = models.CharField(max_length=2, choices=VOCATIONAL_CHOICES, verbose_name='Formación')
+    cellphone = models.PositiveSmallIntegerField(null=True, blank=True, verbose_name='Teléfono')
+    address = models.TextField(max_length=200, null=True, blank=True, verbose_name='Dirección')
+
+    def __str__(self):
+        return "%s" % self.user
+
+    class Meta:
+        ordering = ['user']
+        verbose_name = 'Perfil'
+        verbose_name_plural = 'Perfiles'
