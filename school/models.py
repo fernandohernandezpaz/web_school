08450fda07167d2e11545d0f9cd8fa1e9740e2f2
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2020-02-27 08:49:06 -0600

    creando modelo PersonalFile(Expediente Personal)

--- school/models.py
+++ school/models.py
@@ -1,6 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+GRADE_CHOICES = [
+    ('I', '1er Nivel'),
+    ('II', '2do Nivel'),
+    ('III', '3er Nivel'),
+    ('1er', '1er Grado'),
+    ('2do', '2do Grado'),
+    ('3er', '3er Grado'),
+    ('4to', '4to Grado'),
+    ('5to', '5to Grado'),
+    ('6to', '6to Grado'),
+    ('7mo', '7mo Grado'),
+    ('8vo', '8vo Grado'),
+    ('9no', '9no Grado'),
+    ('10mo', '10mo Grado'),
+    ('11vo', '11vo Grado'),
+]
+
 
 # Create your models here.
 class Nationality(models.Model):
@@ -46,3 +63,28 @@ class Course(models.Model):
     class Meta:
         verbose_name = 'Asignatura'
         verbose_name_plural = 'Asignaturas'
+
+
+class PersonalFile(models.Model):
+    YES_OR_NOT_CHOICES = ((True, 'Si'), (False, 'No'))
+    have_brothers_center = models.BooleanField(choices=YES_OR_NOT_CHOICES, null=False,
+                                               verbose_name='¿Tiene hermanos en el centro?')
+    how_many = models.PositiveSmallIntegerField(null=True, blank=True,
+                                                verbose_name='¿Cuántos?')
+    religion = models.CharField(max_length=20, null=True, blank=False,
+                                verbose_name='Religión')
+    origin_center = models.CharField(max_length=50, null=True, blank=False,
+                                     verbose_name='Centro de Procedencia')
+    year_taken_origin_center = models.CharField(max_length=4, choices=GRADE_CHOICES, null=True,
+                                                verbose_name='Año cursado del centro de procedencia')
+    diseases = models.CharField(max_length=350, null=True, blank=True,
+                                verbose_name='Enfermedades')
+    in_emergencies_call = models.CharField(max_length=350, null=True, blank=True,
+                                           verbose_name='En caso de emergencias llamar a')
+
+    def __str__(self):
+        return self.have_brothers_center
+
+    class Meta:
+        verbose_name = 'Expediente Personal'
+        verbose_name_plural = 'Expedientes Personales'
