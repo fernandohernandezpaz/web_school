8bd1a697b7ff135c18c7e5d8c7b8d15e23db4cae
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2020-03-14 12:26:32 -0600

    identando archivo models.py

--- school/models.py
+++ school/models.py
@@ -40,11 +40,20 @@ class Profile(models.Model):
         ('S', 'Secundaria'),
     ]
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Usuario')
-    document = models.CharField(max_length=16, unique=True, null=False, blank=False, verbose_name='Cedula')
-    vocation = models.CharField(max_length=2, choices=VOCATIONAL_CHOICES, verbose_name='Formación')
-    cellphone = models.PositiveSmallIntegerField(null=True, blank=True, verbose_name='Teléfono')
-    address = models.TextField(max_length=200, null=True, blank=True, verbose_name='Dirección')
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                verbose_name='Usuario')
+    document = models.CharField(max_length=16, unique=True, null=False,
+                                blank=False,
+                                verbose_name='Cedula')
+    vocation = models.CharField(max_length=2,
+                                choices=VOCATIONAL_CHOICES,
+                                verbose_name='Formación')
+    cellphone = models.PositiveSmallIntegerField(null=True,
+                                                 blank=True,
+                                                 verbose_name='Teléfono')
+    address = models.TextField(max_length=200, null=True,
+                               blank=True,
+                               verbose_name='Dirección')
 
     def __str__(self):
         return "%s" % self.user
@@ -56,8 +65,12 @@ class Profile(models.Model):
 
 
 class Course(models.Model):
-    name = models.CharField(max_length=50, unique=True, null=True, blank=True, verbose_name='Nombre')
-    active = models.BooleanField(default=True, verbose_name='Activo')
+    name = models.CharField(max_length=50, unique=True,
+                            null=True,
+                            blank=True,
+                            verbose_name='Nombre')
+    active = models.BooleanField(default=True,
+                                 verbose_name='Activo')
 
     def __str__(self):
         return self.name
