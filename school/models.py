89f513bf7d3f7e6b11ce173a7db82e256418ed17
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2020-03-14 11:39:45 -0600

    correccion de listado de años

--- school/models.py
+++ school/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 
 def year_choices():
-    return [(r, r) for r in range(2019, datetime.date.today().year)]
+    return [(r, r) for r in range(2020, datetime.date.today().year)]
 
 
 # Create your models here.
