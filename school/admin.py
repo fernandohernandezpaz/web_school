11e087b0bdac3f50472ad5eb1eb396f835759f1f
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2020-04-06 21:49:53 -0600

    creando admin class para Matriculation

--- school/admin.py
+++ school/admin.py
@@ -12,19 +12,23 @@ class CatalogsAdmin(admin.ModelAdmin):
     list_display = ('name', 'active')
 
 
+class MatriculationAdmin(admin.ModelAdmin):
+    readonly_fields = ['teaching_year']
+
+
 # Register your models here.
 admin.site.register(Nationality, CatalogsAdmin)
 admin.site.register(Course, CatalogsAdmin)
 admin.site.register(Gender, CatalogsAdmin)
 admin.site.register(Grade, CatalogsAdmin)
+admin.site.register(Section, CatalogsAdmin)
 admin.site.register(Profile)
 admin.site.register(PersonalFile)
 admin.site.register(Student)
 admin.site.register(Family)
-admin.site.register(Matriculation)
+admin.site.register(Matriculation, MatriculationAdmin)
 admin.site.register(PaperCenter)
 admin.site.register(Note)
-admin.site.register(Section)
 admin.site.register(GradeSection)
 admin.site.register(MatriculationGradeSection)
 admin.site.register(CourseGradeSection)
