71dbc0e3d5986d8b0e98f1b4ef1bddba333b192d
JeksonPineda <jeksonpineda@gmail.com>
2020-04-29 15:54:24 -0600

    Creando variable de entorno para intentos de registro, nota minima y limite de reparaciones

--- web_school/constance_settings.py
+++ web_school/constance_settings.py
@@ -5,6 +5,9 @@ from datetime import date
 
 CONSTANCE_CONFIG = {
     'CANTIDAD_ALUMNOS_POR_AULA': (30, 'La cantidad limite de alumnos por cada aula(Grado Sección)', int),
+    'LIMITE_REPARACIONES': (3, 'La cantidad limite de Asignaturas por reparar(Asignatura)', int),
+    'NOTA_MINIMA_APROVADO': (60, 'Nota minima para aprovar una evaluacion(Nota)', int),
+    'INTENTOS_PARA_REGISTRAR_NOTA': (2, 'Numero de intentos para registrar una nota(Nota)', int),
     'FECHA_LIMITE_REGISTRO_I_BIMENSUAL': (date(2020, 6, 29), "Ultima fecha de registro"),
     'FECHA_LIMITE_REGISTRO_II_BIMENSUAL': (date(2020, 8, 29), "Ultima fecha de registro"),
     'FECHA_LIMITE_REGISTRO_III_BIMENSUAL': (date(2020, 10, 29), "Ultima fecha de registro"),
@@ -12,7 +15,9 @@ CONSTANCE_CONFIG = {
 
 }
 CONSTANCE_CONFIG_FIELDSETS = {
-    'General Options': ('CANTIDAD_ALUMNOS_POR_AULA', 'FECHA_LIMITE_REGISTRO_I_BIMENSUAL',
-                        'FECHA_LIMITE_REGISTRO_II_BIMENSUAL', 'FECHA_LIMITE_REGISTRO_III_BIMENSUAL',
-                        'FECHA_LIMITE_REGISTRO_IV_BIMENSUAL'),
+    'General Options': (
+        'CANTIDAD_ALUMNOS_POR_AULA', 'LIMITE_REPARACIONES', 'NOTA_MINIMA_APROVADO', 'INTENTOS_PARA_REGISTRAR_NOTA',
+        'FECHA_LIMITE_REGISTRO_I_BIMENSUAL',
+        'FECHA_LIMITE_REGISTRO_II_BIMENSUAL', 'FECHA_LIMITE_REGISTRO_III_BIMENSUAL',
+        'FECHA_LIMITE_REGISTRO_IV_BIMENSUAL'),
 }
