78b830f8c74540f7b13ba0aa2d39872f5a0a2d19
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2020-05-12 09:29:24 -0600

    cargando datos de la matricula del estudiante para ser actualizados

--- school/urls.py
+++ school/urls.py
@@ -9,6 +9,7 @@ app_name = 'school'
 
 urlpatterns = [
     path('matricula', NewMatriculationFormView.as_view(), name='new_form_matriculation'),
+    path('matricula/<int:id>', NewMatriculationFormView.as_view(), name='matriculation_detail'),
     path('school_space', api.get_school_space, name='api_school_space'),
     path('students', api.get_students, name='api_students'),
     path('matricula/guardar', api.guardar_formulario, name='api_save'),
