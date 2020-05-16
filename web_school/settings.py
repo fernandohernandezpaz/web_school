3a758ab194ea1cf59723f6eccd3cbc938d547ca2
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2020-05-15 21:42:58 -0600

    implementando package.json(npm) para control de dependencias de css y js

--- web_school/settings.py
+++ web_school/settings.py
@@ -134,6 +134,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'node_modules'),
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, '../static/web_school')
 
 # JET dashboard custumization
