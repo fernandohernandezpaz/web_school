8aa1eed85e6669b6bed855dc7abd33fa87ccf397
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2020-07-05 20:42:07 -0600

    validando que el input de notas se habilite si estoy dentro del rango de fecha de inicion y fin del bimensual

--- school/templatetags/own_context_processor.py
+++ school/templatetags/own_context_processor.py
@@ -7,3 +7,8 @@ register = template.Library()
 @register.filter
 def greater_than_today(note_date_limit):
     return date.today() > note_date_limit
+
+
+@register.filter
+def between_the_period(date_start, date_end):
+    return date_start >= date.today() <= date_end
