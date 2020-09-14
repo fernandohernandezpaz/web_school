4476bc9a4be14a1505b07950b2a86507d724ab2d
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2020-09-13 20:29:31 -0600

    agregando nuevas variables al sistema

--- web_school/constance_settings.py
+++ web_school/constance_settings.py
@@ -5,6 +5,10 @@ from datetime import date
 
 year = int(date.today().year)
 
+CONSTANCE_ADDITIONAL_FIELDS = {
+    'image_field': ['django.forms.ImageField', {}]
+}
+
 CONSTANCE_CONFIG = {
     'CANTIDAD_ALUMNOS_POR_AULA': (30, 'La cantidad limite de alumnos por cada aula(Grado Sección)', int),
     'NOTA_MINIMA_APROBADO': (60, 'Nota minima para aprobsar una evaluación(Nota)', int),
@@ -14,4 +18,8 @@ CONSTANCE_CONFIG = {
     'FECHA_LIMITE_REGISTRO_III_BIMENSUAL': (date(year, 10, 29), "Ultima fecha de registro", date),
     'FECHA_LIMITE_REGISTRO_IV_BIMENSUAL': (date(year, 12, 12), "Ultima fecha de registro", date),
     'NOMBRE_DEL_COLEGIO': ('', 'Nombre del Colegio', str),
+    'DIRECCION_DEL_COLEGIO': ('', 'Nombre del Colegio', str),
+    'TELEFONO_DEL_COLEGIO': ('', 'Nombre del Colegio', str),
+    'LOCACION_DEL_COLEGIO': ('', 'Nombre del Colegio', str),
+    'LOGO_DEL_COLEGIO': ('', 'Nombre del Colegio', 'image_field'),
 }
